f183c1337b90467b65b172b43125d5b3c48ef3a7
KvJellyBean <kevinnatan91@gmail.com>
2024-02-25 18:47:39 +0700

    feat(footer): add footer section
    This commit adds a footer section/component to the website.
    The footer component includes A copyright notice and Links to the website's creator github

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+  return (
+    <footer>
+      <p>
+        &copy; 2024 <a href="https://github.com/KvJellyBean">KvJellyBean</a>.
+        All rights reserved.
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
